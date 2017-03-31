5ece91fbb648434337c56a0bfb40b73b2671c97e
wisdompeak <guan.huifeng@gmail.com>
2017-03-31 16:41:44 -0500

    Update Dot.cu

--- Dot/Dot.cu
+++ Dot/Dot.cu
@@ -4,11 +4,10 @@ const int blocksPerGrid = (N+threadsPerBlock-1)/threadsPerBlock;
 
 __global__ void dot( float *a, float *b, float *c )
 {
-  __shared__ float cache[threadsPerBlock];
-
-  int i = threadIdx.x + blockIdx.x*threadsPerBlock;
-  int j = threadIdx.x;
-
+  __shared__ float cache[threadsPerBlock]; // shared memory只给一个block用
+  int i = threadIdx.x + blockIdx.x * blockDim.x;  //用于全局变量的寻址
+  int j = threadIdx.x;  // 用于本block内的shared memory寻址
+  
   cache[j] += a[i]*b[i];
 
   __syncthreads();
@@ -25,3 +24,27 @@ __global__ void dot( float *a, float *b, float *c )
   if (threadIdx.x==0)
     c[blockIdx.x]=cache[0];
 }
+
+/***可以想象成为
+for (blockIdx.x = 1; blockIdx.x<M; blockIdx.x++)
+{
+   __shared__ float cache[threadsPerBlock];
+   for (int threadIdx.x = 1; threadIdx.x<N; threadIdx.x++)
+   {
+       cache[threadIdx.x] = a[threadIdx.x + blockIdx.x * blockDim.x] * b[threadIdx.x + blockIdx.x * blockDim.x];
+   }
+   
+   int k=threadsPerBlock/2;
+   while (k>0)
+   {
+       for (int threadIdx.x = 1; threadIdx.x<N; threadIdx.x++)
+       {
+           if (threadIdx.x<k)  cache[threadIdx.x]+=cache[threadIdx.x+k];
+       }
+       k = k/2;
+   }
+   
+   c[blockIdx.x]=cache[0];
+}
+
+/****
